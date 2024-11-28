c5c4a940a9f455bb4aed3dfda9d3a4e0e37288eb
aadi219 <aaadibadola@gmail.com>
2024-11-28 13:02:59 -0500

    connect to geoLocation API

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import LocationForm from './components/LocationForm';
 
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(API_KEY);
+
 function App() {
   return (
     <>
+      <LocationForm apiKey={API_KEY} />
     </>
   );
 }

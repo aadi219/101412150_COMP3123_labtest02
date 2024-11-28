c574da3d71a51112ac93b3c059c5a299d22677a5
aadi219 <aaadibadola@gmail.com>
2024-11-28 08:51:46 -0500

    create react app
    init react project + install axios, tailwind and daisyui

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import LocationForm from './components/LocationForm';
+
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
